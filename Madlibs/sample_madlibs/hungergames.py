04b39bccfb4c829dce1ed15ed38a6561fd36aa30
Melvin <peepersm@gmail.com>
2020-12-16 23:18:11 -0500

    Guess number

---
+++ Madlibs/sample_madlibs/hungergames.py
@@ -0,0 +1,18 @@
+def madlib():
+    number = input("Number: ")
+    adj = input("Adjective: ")
+    verb = input("Verb: ")
+    verb2 = input("Noun: ")
+    noun = input("Noun: ")
+    noun2 = input("Noun: ")
+    noun3 = input("Noun: ")
+    noun4 = input("Noun: ")
+    noun5 = input("Noun: ")
+    noun_plural = input("Noun: (plural): ")
+    noun_plural_2 = input("Noun: (plural): ")
+    noun_plural_3 = input("Noun: (plural): ")
+    sound_important = input("Name of something that sounds important: ")
+
+    madlib=f"{number} seconds. That's how we're required to {verb} on our metal circles before \
+the sound of a {noun} release us. Step off before the {number} seconds is up, and {noun_plural} blow your \
+"
